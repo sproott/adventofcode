cc822990b77cf7d4ab16b3e0f8fd846a7043dd0a
sproott <david.hrabe12@seznam.cz>
2021-12-05 23:49:04 +0100

    2021/4 in Haskell

--- 2021/4/Haskell/GiantSquid.hs
+++ 2021/4/Haskell/GiantSquid.hs
@@ -1,4 +1,6 @@
+import Data.Function (on)
 import Data.List (scanl', transpose)
+import Data.List.Split (splitOn, splitWhen)
 
 data CellType = Marked | Unmarked deriving (Eq, Show)
 type Cell = (Int, CellType)
@@ -15,10 +17,36 @@ parseBoard :: [String] -> Board
 parseBoard = fmap $ fmap (mkCell . read) . words where
   mkCell n = (n, Unmarked)
 
-runBoards :: [Int] -> [Board] -> (Int, Board)
-runBoards ns boards = let
+parse :: String -> ([Int], [Board])
+parse input = (ns, boards) where
+  (nstr:_:rest) = lines input
+  ns = fmap read $ splitOn "," nstr
+  boards = parseBoard <$> splitWhen null rest
+
+runBoards :: [Int] -> [Board] -> ([Board] -> Bool) -> (Int, Board)
+runBoards ns boards endingCondition = let
   gameSequence :: [(Int, [Board])]
   gameSequence = scanl' (\(_, boards) n -> (n, fmap (markNum n) boards)) (-1, boards) ns
-  lastRound :: (Int, [Board])
-  lastRound = head $ dropWhile (not . any isBoardWinning . snd) gameSequence
-  in (fst lastRound, head $ filter isBoardWinning $ snd lastRound)
+  (gameRounds, (lastRound:_)) = span (not . endingCondition . snd) gameSequence
+  secondLastRound = last gameRounds
+  winningBoard = snd $ head $ filter (\(b1, b2) -> not (isBoardWinning b1) && isBoardWinning b2) $ (zip `on` snd) secondLastRound lastRound
+  in (fst lastRound, winningBoard)
+
+solve :: ([Board] -> Bool) -> [Int] -> [Board] -> Int
+solve endingCondition ns boards = 
+  let (n, board) = runBoards ns boards endingCondition
+  in  n * (sum $ fmap fst $ filter ((== Unmarked) . snd) $ concat board)
+
+solve1 :: [Int] -> [Board] -> Int
+solve1 = solve $ any isBoardWinning
+
+solve2 :: [Int] -> [Board] -> Int
+solve2 = solve $ all isBoardWinning
+
+solveIO :: ([Int] -> [Board] -> Int) -> IO ()
+solveIO solve = do
+  input <- parse <$> readFile "input.txt"
+  print $ uncurry solve $ input
+
+main :: IO ()
+main = solveIO solve1 >> solveIO solve2
